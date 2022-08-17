63562a240afe5950d64f9b009153a63ca6eccb79
Your Name <hsynozcn009@gmail.com>
2022-08-17 21:50:46 +0300

    template engine and static files import

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ import express from "express";
 const app =express();
 const port =3000;
 
+// ejs templete engine
+app.set("view engine","ejs");
+// static files middleware
+app.use(express.static('public'))
+
 app.get("/",(req,res)=>{
-    res.send("Index sayfası 2")
+    res.render("index")
+})
+app.get("/about",(req,res)=>{
+    res.render("about")
 })
 
 app.listen(port,()=>{
